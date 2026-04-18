bc225df145814ffcffca586d5ce5ba8d71488da2
Azat Khaliullin <azatkhaliullin@MacBook-Air-Azat.local>
2026-04-19 00:20:54 +0300

    mesto (отредактированный)

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,7 +1,7 @@
 require('dotenv').config({ path: ['./.env', './.env.deploy'] });
 
 const {
-  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REPOSITORY, DEPLOY_REF = 'origin/main',
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REPOSITORY, DEPLOY_REF = 'origin/master',
 } = process.env;
 
 module.exports = {
