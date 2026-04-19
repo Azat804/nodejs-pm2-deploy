32f87456f602b082260cd0feebf6a079cfdc545a
Azat Khaliullin <azatkhaliullin@MacBook-Air-Azat.local>
2026-04-19 20:43:35 +0300

    mesto (отредактированный)

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,3 +1,3 @@
 export const { JWT_SECRET = 'JWT_SECRET' } = process.env;
 export const { DB_ADDRESS = 'mongodb://localhost:27017/mestodb' } = process.env;
-export const { FRONTEND_URL = 'https://mestoapp.students.nomorepartiessite.ru' } = process.env;
+export const { FRONTEND_URL = 'mestoapp.students.nomorepartiessite.ru' } = process.env;
